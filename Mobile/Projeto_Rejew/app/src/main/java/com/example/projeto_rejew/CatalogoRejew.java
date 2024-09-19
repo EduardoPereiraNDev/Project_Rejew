c9feb4faa5a6e729e318b845c21c2273ba0264fe
RenatoFallaci <renato.fallaci@aluno.senai.br>
2024-09-19 13:54:02 -0300

    Atualizacoes layouts

--- Mobile/Projeto_Rejew/app/src/main/java/com/example/projeto_rejew/CatalogoRejew.java
+++ Mobile/Projeto_Rejew/app/src/main/java/com/example/projeto_rejew/CatalogoRejew.java
@@ -26,7 +26,14 @@ public class CatalogoRejew extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+<<<<<<< HEAD
+                imageView = findViewById(R.id.fotoperfil);
+
+=======
+
         imageView = findViewById(R.id.fotoperfil);
+
+>>>>>>> 816842c3358dc00edc8b7d2c0b14b7dcabc26a06
     }
 
     public void abrirMenu(View v) {
@@ -49,4 +56,3 @@ public class CatalogoRejew extends AppCompatActivity {
         startActivity(intent);
     }
 }
-
