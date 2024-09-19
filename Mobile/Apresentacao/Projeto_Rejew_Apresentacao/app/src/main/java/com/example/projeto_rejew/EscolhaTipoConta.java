ce6c794b21ac3fda31518d94fff617aea625c13e
Eduardo Augusto Radaelli <radaellieduardo7@gmail.com>
2024-09-19 15:27:09 -0300

    EduardoRadaelli_19/09/2024

--- Mobile/Apresentacao/Projeto_Rejew_Apresentacao/app/src/main/java/com/example/projeto_rejew/EscolhaTipoConta.java
+++ Mobile/Apresentacao/Projeto_Rejew_Apresentacao/app/src/main/java/com/example/projeto_rejew/EscolhaTipoConta.java
@@ -16,7 +16,7 @@ public class EscolhaTipoConta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_escolha_tipo_conta);
+        setContentView(R.layout.activity_tipo_conta);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- Mobile/Apresentacao/Projeto_Rejew_Apresentacao/app/src/main/java/com/example/projeto_rejew/FormCadastro.java
+++ Mobile/Apresentacao/Projeto_Rejew_Apresentacao/app/src/main/java/com/example/projeto_rejew/FormCadastro.java
@@ -16,7 +16,7 @@ public class FormCadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_form_cadastro);
+        setContentView(R.layout.activity_form_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
