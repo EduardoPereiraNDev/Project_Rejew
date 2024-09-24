e0818d3644bf3fd47506a1a70b24408f0e066523
Eduardo Pereira Nunes <edu.pnunes07@gmail.com>
2024-09-24 10:41:57 -0300

    Alteracao apresentacao

--- Mobile/Apresentacao/Projeto_Rejew_Apresentacao/app/src/main/java/com/example/projeto_rejew/CatalogoRejew.java
+++ Mobile/Apresentacao/Projeto_Rejew_Apresentacao/app/src/main/java/com/example/projeto_rejew/CatalogoRejew.java
@@ -16,38 +16,62 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CatalogoRejew extends AppCompatActivity {
     ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pagina_inicial);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
     }
 
     public void abrirMenu(View v) {
+        Intent intent = new Intent(CatalogoRejew.this, PerfilUsuario.class);
+        startActivity(intent);
+    }
+
+    /*
+    public void abrirMenu(View v) {
+        imageView = findViewById(R.id.fotoperfil);
         PopupMenu popupMenu = new PopupMenu(CatalogoRejew.this, imageView);
         popupMenu.getMenuInflater().inflate(R.menu.main_menu, popupMenu.getMenu());
 
         popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-
             public boolean onMenuItemClick(MenuItem item) {
-                Toast.makeText(CatalogoRejew.this, item.getItemId() + ", " + item.getTitle(), Toast.LENGTH_SHORT).show();
-                return true;
+               switch (item.getItemId()) {
+                    case R.id.ver_perfil:
+                        Toast.makeText(CatalogoRejew.this, "Opção 1 selecionada", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.configuração:
+                        Toast.makeText(CatalogoRejew.this, "Opção 2 selecionada", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.sair_conta:
+                        sairConta(v);
+                        return true;
+                    default:
+                        return false;
+                }
             }
         });
 
         popupMenu.show();
     }
+*/
+    public void passarTelaTerror(View v) {
+        Intent intent = new Intent(CatalogoRejew.this, Categorias.class);
+        startActivity(intent);
+    }
 
-    public void sairConta(MenuItem item) {
+    public void sairConta(View v) {
         Intent intent = new Intent(CatalogoRejew.this, MainActivity.class);
         startActivity(intent);
     }
-}
 
+    public void sairConta(MenuItem item) {
+    }
+}
